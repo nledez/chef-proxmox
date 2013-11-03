368b7fec36693a39aa0f91f5a8836364c4f11d16
Nicolas Ledez <github@ledez.net>
2013-11-03 16:00:53 +0100

    Create /var/lib/vz as LVM

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,3 +9,4 @@
 
 default['proxmox']['packages-stage1'] = %w{pve-firmware pve-kernel-2.6.32-23-pve pve-headers-2.6.32-23-pve}
 default['proxmox']['packages'] = %w{proxmox-ve-2.6.32 ksm-control-daemon vzprocps}
+default['proxmox']['lvm'] = true
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,37 @@
 # All rights reserved - Do Not Redistribute
 #
 
+if node['proxmox']['lvm']
+  directory "/var/lib/vz" do
+    action :create
+  end
+
+  LVM = `lvs --noheadings`
+  volumes = LVM.split(/\n/).map { |v| v.split(/ +/)[1] }
+  unless volumes.include? 'vz'
+    Chef::Log.info "Need to create vz lv"
+    vg = LVM.split(/\n/).map { |v| v.split(/ +/)[2] }.uniq[0]
+
+    bash "create-/dev/#{vg}/vz" do
+      code <<-EOH
+      /sbin/lvcreate -L 10G --name vz #{vg} && /sbin/mkfs.ext3 /dev/#{vg}/vz
+      /bin/mv /var/lib/vz /var/lib/vz.bak ; /bin/mkdir /var/lib/vz
+      EOH
+    end
+
+    mount "/var/lib/vz" do
+      fstype "ext3"
+      device "/dev/#{vg}/vz"
+      options "defaults,noatime,nodiratime"
+      action   [:mount, :enable]
+    end
+
+    bash "finish-with-/var/lib/vz.bak-move" do
+      code "/bin/mv /var/lib/vz.bak/* /var/lib/vz/ ; /bin/rmdir /var/lib/vz.bak"
+    end
+  end
+end
+
 file '/etc/grub.d/06_OVHkernel' do
   action :delete
 end
